e0e5c1314df726739e9b61fdee4456db4e7e62b7
Syifaazzah <sifaazzahra2612@gmail.com>
2024-07-02 16:00:22 +0700

    edit footer

--- footer.php
+++ footer.php
@@ -29,7 +29,6 @@
             <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
             <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
             <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
             </div>
         </div>
 
@@ -44,25 +43,15 @@
             </ul>
         </div>
 
-        <div class="col-lg-2 col-6 footer-links">
-            <h4>Our Services</h4>
-            <ul>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Web Design</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Web Development</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Product Management</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Marketing</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Graphic Design</a></li>
-            </ul>
-        </div>
 
         <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-            <h4>Contact Us</h4>
+            <h4>Hubungi Kami</h4>
             <p>
-            A108 Adam Street <br>
-            New York, NY 535022<br>
+            Jl. Arcadia No.60 <br> 
+            Tangerang, Banten 15154<br>
             United States <br><br>
-            <strong>Phone:</strong> +1 5589 55488 55<br>
-            <strong>Email:</strong> info@example.com<br>
+            <strong>Phone:</strong> +62 882-9532-7727<br>
+            <strong>Email:</strong> paopaobapao@gmail.com<br>
             </p>
 
         </div>
